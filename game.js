23a16414961270d9d36ced88975fe55d83d18269
vsaucek89 <vsaucek8989@gmail.com>
2022-07-03 17:28:57 -0500

    Finished game

--- game.js
+++ game.js
@@ -1,4 +1,66 @@
-const playerslection = 'rock'
-const computerslection = 'rock';
-let computerScore = 0;
-let playerComputer = 0;
+let playerScore = 0
+let compScore = 0
+game()
+
+
+
+
+
+
+function game(){
+    for(let i = 0;i<5;i++){
+        let playerChoice = prompt("rock, paper, or scissors?")
+        let computerChoice = computerPlay()
+        console.log(play(playerChoice,computerChoice))
+        console.log(playerScore+"-"+compScore)
+    }
+    if(playerScore == compScore){
+        console.log("the game is a tie.")
+    }else if(playerScore > compScore){
+        console.log("you win the game!!!!")
+    }else{
+        console.log("you lose the game")
+    }
+}
+
+function computerPlay() {
+    let compNum = Math.random()*3 
+    if(0 <= compNum && compNum < 1){
+        return "rock"
+    }else if(1 <= compNum && compNum < 2){
+        return "paper"
+    }else{
+        return "scissors"
+    }
+}
+
+function play(playersel, compsel){
+    let playsel = playersel.toLowerCase()
+    if(playsel == compsel){
+        return "tie"
+    }else if(playsel == "rock"){
+        if(compsel == "paper"){
+            compScore++
+            return "you lose!"
+        }else if(compsel == "scissors"){
+            playerScore++
+            return "you win!"
+        }
+    }else if(playsel == "paper"){
+        if(compsel == "scissors"){
+            compScore++
+            return "you lose!"
+        }else if(compsel == "rock"){
+            playerScore++
+            return "you win!"
+        }
+    }else{
+        if(compsel == "rock"){
+            compScore++
+            return "you lose!"
+        }else{
+            playerScore++
+            return "you win"
+        }
+    }
+}
